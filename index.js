3fe8ee51105178ca6575ec335bba16e05a64d054
camp0609 <35385718+camp0609@users.noreply.github.com>
2021-09-28 21:56:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,9 @@ const findPath = require('./pathFinder.js');
 const express = require('express');
 const app = express();
 
-
-//const countries = [can, usa, mex, blz, gtm, slv, hnd, nic, cri, pan];
-
-// function findPathHelper(queue, finalCountry, visited) {
-//     usa.neighbors.forEach(n => {
-//         if(finalCountry == n) {
-//             n.prev = current;
-//             break;
-//         }
-//     })
-//     currentCountry = countries.find(c => c.id == currentCountry);
-// }
-
-// function findPath(country) {
-//     let visited = new Set();
-//     let paths = [];
-//     let queue = new Queue();
-//     queue.enqueue(usa);
-//     return countries.find(c => c.id == country);
-// }
-
 app.get('/:id', (req, res) => {
     res.send(findPath(req.params.id));
 });
 
-app.listen(3000, () => console.log('connected to port 3000'));
+const port = process.env.port || 3000;
+app.listen(port, () => console.log('connected'));
